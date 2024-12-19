87f4ce12e30ef9964435c8781960f0fdc960d415
John Tettis <johndavidtettis@gmail.com>
2024-12-19 15:27:58 -0500

    added migration files. need to fill out. boring!

---
+++ migrations/20241219200807_create_hats_table.js
@@ -0,0 +1,23 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function(knex) {
+    return knex.schema.createTable('hats', table => {
+        table.increments('id').primary();
+        table.string('name').notNullable();
+        table.string('primary_color').notNullable();
+        table.string('material').notNullable();
+        table.integer('brim_size').notNullable();
+        table.integer('height').notNullable();
+        table.decimal('price').notNullable();
+      })
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function(knex) {
+  return knex.schema.dropTable('hats');
+};
