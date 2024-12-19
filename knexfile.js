d8143cbb3c7b5576c3ee06abac330e111bfa8b63
John Tettis <johndavidtettis@gmail.com>
2024-12-19 16:53:24 -0500

    updated to use postgres in development

--- knexfile.js
+++ knexfile.js
@@ -6,10 +6,20 @@ require('dotenv').config();
 module.exports = {
 
   development: {
-    client: 'sqlite3',
+    client: 'postgresql',
     connection: {
-      filename: './dev.sqlite3'
+      database: 'happy-little-caps-dev',
+      user:     process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
     }
+    
   },
 
   staging: {
