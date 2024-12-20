3dcfbf1486e965f25534edec2001e92a13ba3251
John Tettis <johndavidtettis@gmail.com>
2024-12-19 21:37:46 -0500

    added authentication middleware

--- pages/api/hats/[hatId].ts
+++ pages/api/hats/[hatId].ts
@@ -1,4 +1,5 @@
 import db from '@/lib/knex';
+import {authenticate} from '@/lib/auth';
 
 export default async function handler(req:any, res:any) {
   switch(req.method) {
@@ -14,18 +15,20 @@ export default async function handler(req:any, res:any) {
    }
    case 'PATCH': {
     try {
+      //authenticate admin
+      const admin = authenticate(req); 
       // Validate request body using the schema
       const { ...updates } = req.body;
-      const { id } = req.query;
+      const { hatId } = req.query;
 
       // Ensure `id` is provided
-      if (!id) {
+      if (!hatId) {
         return res.status(400).json({ error: 'ID is required for updates.' });
       }
 
       // Perform the partial update in the database
       const updatedHat = await db('hats')
-        .where({ id })
+        .where({ id :hatId})
         .update(updates)
         .returning('*');
 
@@ -35,8 +38,8 @@ export default async function handler(req:any, res:any) {
 
       res.status(200).json(updatedHat);
     } catch (error) {
-      console.error('Error updating product:', error);
-      res.status(500).json({ error: 'Failed to update product' });
+      console.error('Error updating hat:', error);
+      res.status(500).json({ error: 'Failed to updatehat' });
     }
     break;
   }
