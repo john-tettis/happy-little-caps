1702b0c45033f24f31fc77bb20c3e49036d1b3f3
John Tettis <johndavidtettis@gmail.com>
2024-12-19 21:38:04 -0500

    renamed user to admin

--- pages/api/admin/token/index.ts
+++ pages/api/admin/token/index.ts
@@ -1,7 +1,7 @@
 // pages/api/auth/login.js
 
 import { generateToken } from '@/lib/jwt';
-import bcrypt from 'bcryptjs';
+import * as bcrypt from 'bcryptjs';
 import db from '@/lib/knex'; // Your Knex setup
 
 export default async function handler(req:any, res:any) {
@@ -10,19 +10,19 @@ export default async function handler(req:any, res:any) {
 
     try {
       // Find user from the database
-      const user = await db('admin').where('username', username).first();
-      if (!user) {
+      const admin = await db('admin').where('username', username).first();
+      if (!admin) {
         return res.status(400).json({ error: 'Invalid credentials' });
       }
 
       // Compare the password with the hashed password in the database
-      const isPasswordValid = await bcrypt.compare(password, user.password);
+      const isPasswordValid = await bcrypt.compare(password, admin.password);
       if (!isPasswordValid) {
         return res.status(400).json({ error: 'Invalid credentials' });
       }
 
       // Generate the JWT token
-      const token = generateToken(user);
+      const token = generateToken(admin);
       res.status(200).json({ message: 'Login successful', token });
     } catch (error) {
       console.error('Error during login:', error);
