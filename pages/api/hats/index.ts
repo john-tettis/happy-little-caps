8121e0aeb70b2a94824a32a0beb44c5a39bc09b0
John Tettis <johndavidtettis@gmail.com>
2024-12-19 20:33:44 -0500

    switch case / post

--- pages/api/hats/index.ts
+++ pages/api/hats/index.ts
@@ -1,13 +1,39 @@
 import db from '@/lib/knex';
 
+
+
 export default async function handler(req:any, res:any) {
-  if (req.method === 'GET') {
-    try {
-      const hats = await db('hats').select('*');
-      res.status(200).json(hats);
-    } catch (error) {
-      console.error('Error fetching products:', error);
-      res.status(500).json({ error: 'Failed to fetch products' });
+  switch (req.method) {
+    case 'GET' : {
+      try {
+        const hats = await db('hats').select('*');
+        res.status(200).json(hats);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+        res.status(500).json({ error: 'Failed to fetch products' });
+      }
     }
+    case "POST": {
+      try {
+        const { 
+          name,
+          price,
+          quantity,
+          description,
+          material, 
+          primary_color,
+          image_url
+        } = req.body;
+        const hat = await db('hats').insert({ name, price, quantity, description, material, primary_color,image_url}).returning('*');
+        res.status(201).json(hat);
+      } catch (error) {
+          console.error('Error creating product:', error);
+          res.status(500).json({ error: 'Failed to create product' });
+      }
+    }
+  
+    default:
+      res.setHeader('Allow', ['GET', 'POST']);
+      res.status(405).json({ error: `Method ${req.method} not allowed` });
   }
 }
